7b0d82ed179e48ae04e4555980371daee9b4e21d
Jerry <i@sjis.me>
2024-08-09 11:55:50 +0800

    feat: callback update product info

--- app/api/wxnotify/route.ts
+++ app/api/wxnotify/route.ts
@@ -1,7 +1,7 @@
 import { type NextRequest } from 'next/server'
 
 import { getHash } from '@/lib/xunhu_pay'
-import { prisma } from '@/prisma'
+import { prisma, startTransaction } from '@/prisma'
 
 export async function POST(request: NextRequest) {
   const formData = await request.formData()
@@ -20,11 +20,56 @@ export async function POST(request: NextRequest) {
   }
 
   const orderId = formData.get('trade_order_id')
+
   if (!orderId) {
     return new Response('order_id error', {
       status: 400,
-    });
+    })
   }
+
+  startTransaction(async (prisma) => {
+    const order = await prisma.orders.findUnique({
+      where: {
+        order_id: orderId.toString(),
+      },
+      include: {
+        product: true,
+      },
+    })
+
+    if (!order) {
+      return new Response('order not found', {
+        status: 400,
+      })
+    }
+    const kami = JSON.parse(order.product.kami) as string[]
+
+    if (kami.length < order.num) {
+      return new Response('kami not enough', {
+        status: 500,
+      })
+    }
+    const restKami = kami.slice(0, order.num)
+
+    prisma.products.updateMany({
+      where: {
+        id: order.product_id,
+      },
+      data: {
+        num: order.product.num - order.num,
+        kami: JSON.stringify(kami.slice(order.num, kami.length)),
+      },
+    })
+    prisma.orders.updateMany({
+      where: {
+        order_id: orderId.toString(),
+      },
+      data: {
+        status: 1,
+        kami: restKami.join('\n'),
+      },
+    })
+  })
 
   const orders = await prisma.orders.updateMany({
     where: {
