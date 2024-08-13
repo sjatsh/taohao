2c562c01b43edfb2f4765deba115aa8e8196c476
Jerry <i@sjis.me>
2024-08-13 21:41:49 +0800

    feat: admin page

--- server/routers/middlewares/auth.ts
+++ server/routers/middlewares/auth.ts
@@ -5,6 +5,7 @@ import { ADMIN_PASSWORD } from '@/env/server'
 export const isLogin = middleware(async (opts) => {
   const { ctx } = opts
 
+  // @ts-ignore
   if (opts.rawInput?.password != ADMIN_PASSWORD) {
     throw new TRPCError({
       code: 'UNAUTHORIZED',
