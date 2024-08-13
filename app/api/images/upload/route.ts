9d2d2ef850d60be440cb335b7622b4e991e9054e
Jerry <i@sjis.me>
2024-08-13 21:31:58 +0800

    feat: admin page

--- app/api/images/upload/route.ts
+++ app/api/images/upload/route.ts
@@ -4,6 +4,7 @@ import { NextRequest, NextResponse } from 'next/server'
 export async function POST(request: NextRequest) {
   const { searchParams } = new URL(request.url)
   const filename = searchParams.get('filename')
+  //@ts-ignore
   const blob = await put('images/' + filename, request.body, {
     access: 'public',
   })
