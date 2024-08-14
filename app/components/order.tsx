f6c2949e62961e72de8a6dc7b6db548f544fc478
Jerry <i@sjis.me>
2024-08-14 16:50:07 +0800

    feat: change products list style

--- app/components/order.tsx
+++ app/components/order.tsx
@@ -31,7 +31,7 @@ export const OrderCard: FC<OrderCardProps> = (props: OrderCardProps) => {
           )}
         </CardHeader>
         <Skeleton isLoaded={loaded}>
-          <CardBody className="h-[180px] w-[192px] overflow-visible p-5">
+          <CardBody className="min-h-[140px] w-[160px] overflow-visible p-5">
             <Image
               className="object-cover"
               radius="lg"
