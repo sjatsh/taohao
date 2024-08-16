8217bc59d4dfba59c186d96a11f6fa39ebfce7b9
Jerry <i@sjis.me>
2024-08-16 14:08:52 +0800

    feat: sitemap

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,5 +1,18 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
   siteUrl: process.env.SITE_URL,
+  changefreq: 'daily',
+  priority: 0.7,
+  sitemapSize: 5000,
   generateRobotsTxt: true,
+  // Default transformation function
+  transform: async (config, path) => {
+    return {
+      loc: path, // => this will be exported as http(s)://<config.siteUrl>/<path>
+      changefreq: config.changefreq,
+      priority: config.priority,
+      lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+      alternateRefs: config.alternateRefs ?? [],
+    }
+  },
 }
