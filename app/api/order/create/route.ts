a8166b68bb7a8bfa3a91caf8938e8a0c11091206
Jerry <i@sjis.me>
2024-10-18 00:39:33 +0800

    feat: xianyu

--- app/api/order/create/route.ts
+++ app/api/order/create/route.ts
@@ -35,7 +35,7 @@ export async function POST(request: NextRequest) {
 
   const product = await prisma.products.findUnique({
     where: {
-      id: productId.toString(),
+      id: Number(productId),
     },
   })
 
