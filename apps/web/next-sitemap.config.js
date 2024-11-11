d6bf7080905de36c109095fa89a5476a94182fa6
Jerry <i@sjis.me>
2024-11-11 21:27:09 +0800

    feat: change sitmap config

--- apps/web/next-sitemap.config.js
+++ apps/web/next-sitemap.config.js
@@ -1,13 +1,13 @@
 /** @type {import('next-sitemap').IConfig} */
 module.exports = {
-  siteUrl: process.env.SITE_URL,
+  siteUrl: process.env.NEXT_PUBLIC_SITE_URL,
   changefreq: 'always',
   autoLastmod: true,
   generateRobotsTxt: true,
   generateIndexSitemap: true,
   exclude: ['/admin/*'],
   robotsTxtOptions: {
-    additionalSitemaps: [process.env.SITE_URL + '/orders/buy/sitemap.xml'],
+    additionalSitemaps: [process.env.NEXT_PUBLIC_SITE_URL + '/orders/buy/sitemap.xml'],
   },
   transform: async (config, path) => {
     return {
