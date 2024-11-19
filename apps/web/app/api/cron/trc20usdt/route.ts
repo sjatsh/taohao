6e301771f93f638e37c9c79d01915d92a5927eea
Jerry <i@sjis.me>
2024-11-20 01:38:23 +0800

    feat: change log

--- apps/web/app/api/cron/trc20usdt/route.ts
+++ apps/web/app/api/cron/trc20usdt/route.ts
@@ -3,9 +3,11 @@ import { logger } from '@taohao/logger'
 import { CRON_SECRET } from '@taohao/env/src/server'
 
 export async function GET(req: NextRequest) {
-  logger.info('cron trc20usdt')
+  logger.info({msg: "Cron job is running"})
   if (req.headers.get('Authorization') !== `Bearer ${CRON_SECRET}`) {
+    logger.warn({error: "Unauthorized"})
     return NextResponse.next({ status: 401, statusText: 'Unauthorized' })
   }
+  logger.info({msg: "Cron job is authorized"})
   return NextResponse.json({ ok: true })
 }
