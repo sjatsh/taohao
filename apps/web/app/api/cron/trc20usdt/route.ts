2a98b35fdba2ca3e5694de8068118a7ecb9a79d8
Jerry <i@sjis.me>
2024-12-06 21:04:04 +0800

    feat: vercel.json

--- apps/web/app/api/cron/trc20usdt/route.ts
+++ apps/web/app/api/cron/trc20usdt/route.ts
@@ -1,10 +1,9 @@
 import { NextRequest, NextResponse } from 'next/server'
 import { logger } from '@taohao/logger'
-import { CRON_SECRET } from '@taohao/env/src/server'
 
 export async function GET(req: NextRequest) {
   logger.info({msg: "Cron job is running"})
-  if (req.headers.get('Authorization') !== `Bearer ${CRON_SECRET}`) {
+  if (req.headers.get('Authorization') !== `Bearer ${process.env.CRON_SECRET}`) {
     logger.warn({error: "Unauthorized"})
     return NextResponse.next({ status: 401, statusText: 'Unauthorized' })
   }
